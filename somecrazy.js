cf00c6edc0b2597cd1baf4bdf7a91e5821cd18dd
Manoj Reddy <manureddy.g94@gmail.com>
2021-01-06 18:27:04 +0530

    Update somecrazy.js

--- somecrazy.js
+++ somecrazy.js
@@ -3,13 +3,16 @@
    <h2>Hello im H2</h2>
  </Container>
  
- export const Container = (props) => {
+export const Container = (props) => {
 const {children} = props; 
   const modifiedChilds = React.Children.map(children, (child,index) => {
     console.log("Each  child is", child,index);
+    const isLast = index === children.length - 1
     return (
       <div style={{border: `${(index + 1) * 10}px solid red`}}>
-        {React.cloneElement(child)}
+        
+        
+        {isLast ? React.cloneElement(child,{style:{"color":"blue"}}) : <span>{child} </span>}
       </div>
     )
   })
